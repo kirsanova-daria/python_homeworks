b33ecf81412965725a0bf19a541ec4f8f505cd66
kirsanova-daria <54403984+kirsanova-daria@users.noreply.github.com>
2022-10-14 07:40:11 +0300

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,95 +1,51 @@
-import csv
+class CountVectorizer:
+    """gets unique words from input, creates document-term matrix"""
+    stop_words = ("the", "a", "and")
 
-file_in = "Corp_Summary.csv"
-file_out = 'hw2.csv'
+    def __init__(self, input=None, lowercase=True):
+        if type(input) == list:
+            self.input = input
+        else:
+            raise ValueError
+        self.lowercase = lowercase
+        self._vocabulary = {}
 
-
-def choice(file1: str, file2: str) -> dict:
-    """Функция выбора команды"""
-    inquiry = input()
-    if inquiry == '1':
-        return get_hierarchy(file1)
-    elif inquiry == '2':
-        return get_summary(file1)
-    else:
-        return write_summary(file1, file2)
-
-
-def get_hierarchy(file: str) -> dict:
-    """Функция выводит иерархию команд (департаменты и их команды)"""
-    with open(file, newline='') as csvfile:
-        f_read = csv.reader(csvfile, delimiter=';', quotechar='|')
-        next(f_read)
-        ans = dict()
-        for row in f_read:
-            if row[2] not in ans.get(row[1], list()):
-                ans.setdefault(row[1], list()).append(row[2])
-
-    print('{:<13} {}'.format('Департамент', 'Команды'))
-    for key, value in ans.items():
-        print('{:<13} {}'.format(key, ', '.join(value)))
-    return ans
-
-
-def get_summary(file: str) -> dict:
-    """Функция выводит сводный отчёт по департаментам: название, численность,
-    "вилка" зарплат в виде мин – макс, среднюю зарплату"""
-    with open(file, newline='') as csvfile:
-        f_read = csv.reader(csvfile, delimiter=';', quotechar='|')
-        next(f_read)
-        ans = dict()
-        for row in f_read:
-            sal = int(row[-1])
-            ans.setdefault(row[1], [0, 1000000, 0, 0])[0] += 1
-            if sal < ans.setdefault(row[1], [0, 1000000, 0, 0])[1]:
-                ans.setdefault(row[1], [0, 1000000, 0, 0])[1] = sal
+    def fit_transform(self):
+        """learns vocabulary dictionary, returns document-term matrix"""
+        output = []
+        max_i = 0
+        for i, line in enumerate(self.input):
+            if i != 0:
+                output.append([0 for i in range(len(output[i-1]))])
             else:
-                if sal > ans.setdefault(row[1], [0, 1000000, 0, 0])[2]:
-                    ans.setdefault(row[1], [0, 1000000, 0, 0])[2] = sal
-            ans.setdefault(row[1], [0, 1000000, 0, 0])[-1] += sal
-    print('{:<13} {:>12} {:>18}   {}'.format(
-        'Департамент', 'Численность', 'Зарплатная вилка', 'Средняя зарплата'
-        ))
-    for key, value in ans.items():
-        print('{:<13} {:>12} {:>9} - {}  {:>17}'.format(
-            key, value[0], value[1], value[2], round(value[-1]/value[0], 2)
-            ))
-    return ans
+                output.append([])
+            for word in line.split():
+                if self.lowercase:
+                    word = word.lower()
+                if word not in self.stop_words:
+                    if word not in self._vocabulary:
+                        self._vocabulary[word] = max_i
+                        for elem in output[:-1]:
+                            elem.append(0)
+                        max_i += 1
 
+                        output[i].append(1)
+                    else:
+                        output[i][self._vocabulary[word]] += 1
+        return output
 
-def write_summary(file1: str, file2: str) -> dict:
-    """Функция записывает в csv файл  сводный отчёт по департаментам: название,
-    численность, "вилка" зарплат в виде мин – макс, среднюю зарплату"""
-    with open(file1, newline='') as csvfile:
-        f_read = csv.reader(csvfile, delimiter=';', quotechar='|')
-        next(f_read)
-        ans = dict()
-        for row in f_read:
-            sal = int(row[-1])
-            ans.setdefault(row[1], [0, 1000000, 0, 0])[0] += 1
-            if sal < ans.setdefault(row[1], [0, 1000000, 0, 0])[1]:
-                ans.setdefault(row[1], [0, 1000000, 0, 0])[1] = sal
-            else:
-                if sal > ans.setdefault(row[1], [0, 1000000, 0, 0])[2]:
-                    ans.setdefault(row[1], [0, 1000000, 0, 0])[2] = sal
-            ans.setdefault(row[1], [0, 1000000, 0, 0])[-1] += sal
-    with open(file2, 'w') as csvfile:
-        fieldnames = [
-            'Департамент', 'Численность', 'Зарплатная вилка',
-            'Средняя зарплата'
-            ]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for key, value in ans.items():
-            writer.writerow({
-                'Департамент': key,
-                'Численность': value[0],
-                'Зарплатная вилка': '-'.join(map(str, value[1:2])),
-                'Средняя зарплата': round(value[-1]/value[0], 2)
-                })
-    print("Writing complete")
-    return ans
+    def get_feature_names(self):
+        """returns unique words from input"""
+        return self._vocabulary.keys()
 
 
 if __name__ == '__main__':
-    choice(file_in, file_out)
+    corpus = [
+        'Crock Pot Pasta Never boil pasta again',
+        'Pasta Pomodoro Fresh ingredients Parmesan to taste',
+        'pasta pasta pasta pasta pasta pasta pasta pasta pasta'
+        ]
+    vectorizer = CountVectorizer(input=corpus)
+    count_matrix = vectorizer.fit_transform()
+    print(vectorizer.get_feature_names())
+    print(count_matrix)
